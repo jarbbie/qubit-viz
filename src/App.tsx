adabec3a4176821bc91b8125ca38d97207a2a519
jb <peeranat.ngam@gmail.com>
2026-07-10 00:42:53 +0700

    feat: install typescript for tsc

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-
 function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
